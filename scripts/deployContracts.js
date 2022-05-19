15befd39152fd61e7c97bf4af3d22048e8845212
codeTIT4N <lokeshkumar636@yahoo.com>
2022-05-20 02:55:08 +0530

    updated deploy script

--- scripts/deployContracts.js
+++ scripts/deployContracts.js
@@ -8,6 +8,10 @@ async function main() {
     const primarySales = await NalndaBooksPrimarySales.deploy(process.env.NALNDA_ERC20);
     await primarySales.deployed();
     console.log("NalndaBooksPrimarySales deployed to:", primarySales.address);
+    const NalndaBooksSecondarySales = await hre.ethers.getContractFactory("NalndaBooksSecondarySales");
+    const secondarySales = await NalndaBooksSecondarySales.deploy(process.env.NALNDA_ERC20);
+    await secondarySales.deployed();
+    console.log("NalndaBooksSecondarySales deployed to:", secondarySales.address);
 }
 
 main()
