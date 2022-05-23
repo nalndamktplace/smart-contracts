d09cdb97e76f586275fa2ebe4d01417b388a2739
z3t4z00k <zetazook@gmail.com>
2022-05-23 15:14:58 +0530

    Switched to tab indentation

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+	const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+	for (const account of accounts) {
+		console.log(account.address);
+	}
 });
 
 // You need to export an object to set up your config
@@ -18,27 +18,27 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.13",
-  defaultNetwork: "hardhat",
-  networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`${process.env.PRIVATE_KEY}`],
-      gas: 3000000,
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`${process.env.PRIVATE_KEY}`],
-    },
-    mumbai: {
-      url: `https://matic-mumbai.chainstacklabs.com`,
-      accounts: [`${process.env.PRIVATE_KEY}`],
-    },
-    hardhat: {
-      gas: 12000000,
-      blockGasLimit: 0x1fffffffffffff,
-      allowUnlimitedContractSize: true,
-      timeout: 1800000,
-    },
-  }
+	solidity: "0.8.13",
+	defaultNetwork: "hardhat",
+	networks: {
+		rinkeby: {
+			url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+			accounts: [`${process.env.PRIVATE_KEY}`],
+			gas: 3000000,
+		},
+		ropsten: {
+			url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+			accounts: [`${process.env.PRIVATE_KEY}`],
+		},
+		mumbai: {
+			url: `https://matic-mumbai.chainstacklabs.com`,
+			accounts: [`${process.env.PRIVATE_KEY}`],
+		},
+		hardhat: {
+			gas: 12000000,
+			blockGasLimit: 0x1fffffffffffff,
+			allowUnlimitedContractSize: true,
+			timeout: 1800000,
+		},
+	}
 };
