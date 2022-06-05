6c6befa5cfbfe96e7741257dec5e684022eb6ce2
codeTIT4N <lokeshkumar636@yahoo.com>
2022-06-05 19:44:52 +0530

    updated solidity version

--- contracts/interfaces/INalndaMarketplace.sol
+++ contracts/interfaces/INalndaMarketplace.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.14;
 
 interface INalndaMarketplace {
     function NALNDA() external view returns (address);
--- hardhat.config.js
+++ hardhat.config.js
@@ -21,7 +21,7 @@ module.exports = {
 	solidity: {
 		compilers: [
 			{
-				version: "0.8.13",
+				version: "0.8.14",
 				settings: {
 					optimizer: {
 						enabled: true,
