2f08e712274a7dd37269492eb097b77888bae118
z3t4z00k <zetazook@gmail.com>
2022-07-01 08:58:16 +0530

    Added transfer ownership to Nalnda

--- scripts/deployContracts.js
+++ scripts/deployContracts.js
@@ -15,7 +15,14 @@ async function main() {
     console.log("NalndaDiscountV1 deployed to:", discount_v1.address);
     console.log("setting address for discount contract...");
     await marketplace.setDiscountContract(discount_v1.address);
-    console.log("done...");
+    console.log("transferring NalndaMarketplace ownership to Nalnda...");
+	const transfer1 = await marketplace.transferOwnership('0x8d0334729755000837dE40D78AeAD13A32388747')
+	const tx1 = await transfer1.wait()
+	console.log("NalndaMarketplace transferred")
+	console.log("transferring NalndaDiscountV1 ownership to Nalnda...");
+	const transfer2 = await discount_v1.transferOwnership('0x8d0334729755000837dE40D78AeAD13A32388747')
+	const tx2 = await transfer2.wait()
+	console.log("NalndaDiscountV1 transferred")
 }
 
 main()
