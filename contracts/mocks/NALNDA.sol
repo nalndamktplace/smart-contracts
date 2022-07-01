74d7b00a20d6ac259677e86e98fa5fb8f9eaf425
codeTIT4N <lokeshkumar636@yahoo.com>
2022-07-01 10:05:47 +0530

    changed the NALNDA to USDC

--- contracts/mocks/NALNDA.sol
+++ contracts/mocks/NALNDA.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Nalnda is ERC20, Ownable {
-    constructor() ERC20("Nalnda", "NALNDA") {}
+    constructor() ERC20("Nalnda USDC Token", "USDC") {}
 
     function mint(uint256 amount) public {
         _mint(_msgSender(), amount);
