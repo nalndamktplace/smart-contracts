3913aa2a493fa015d9424436792b0ff394a51eea
codeTIT4N <lokeshkumar636@yahoo.com>
2022-12-07 21:28:33 +0530

    small additions

--- scripts/deployContractsHedera.js
+++ scripts/deployContractsHedera.js
@@ -2,14 +2,14 @@ const hre = require("hardhat");
 require("dotenv").config();
 
 async function main() {
-    console.log("Using Nalnda ERC20 deployed to:", process.env.NALNDA_ERC20);
+    console.log("Using Nalnda ERC20 deployed to:", process.env.NALNDA_ERC20_HEDERA);
     //deploy NalndaBooksPrimarySales
     const NalndaBooksPrimarySales = await hre.ethers.getContractFactory("NalndaBooksPrimarySales");
-    const primarySales = await NalndaBooksPrimarySales.deploy(process.env.NALNDA_ERC20);
+    const primarySales = await NalndaBooksPrimarySales.deploy(process.env.NALNDA_ERC20_HEDERA);
     await primarySales.deployed();
     console.log("NalndaBooksPrimarySales deployed to:", primarySales.address);
     const NalndaBooksSecondarySales = await hre.ethers.getContractFactory("NalndaBooksSecondarySales");
-    const secondarySales = await NalndaBooksSecondarySales.deploy(process.env.NALNDA_ERC20);
+    const secondarySales = await NalndaBooksSecondarySales.deploy(process.env.NALNDA_ERC20_HEDERA);
     await secondarySales.deployed();
     console.log("NalndaBooksSecondarySales deployed to:", secondarySales.address);
 }
