a441bcc13bd6af35a3887ca789e5c7e67b5688f7
thevirinchi <zetazook@gmail.com>
2022-12-29 18:47:07 +0530

    Temp

---
+++ arguments.js
@@ -0,0 +1,9 @@
+// module.exports = ['0xa948dc08f217C96c687dC3bF2E6051FB906b872A', '0x7306aC7A32eb690232De81a9FFB44Bb346026faB', '0x119df1582e0dd7334595b8280180f336c959f3bb']
+module.exports = [
+    '0xB7647eF82c0179CB1EC6f1D84ACCdb6e9F5Cb823',
+    'https://shiva.ipfs.nalnda.com/ipfs/file?cid=QmcF3NF7qBVPrYrRTHH867CqnifMRfyay6fLwCeZSeX9xc',
+    3200000,
+    0,
+    0,
+    [17, 26],
+]
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,16 @@
-require("@nomiclabs/hardhat-waffle");
-require("dotenv").config();
+require('@nomiclabs/hardhat-etherscan')
+require('@nomiclabs/hardhat-waffle')
+require('dotenv').config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-	const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners()
 
-	for (const account of accounts) {
-		console.log(account.address);
-	}
-});
+    for (const account of accounts) {
+        console.log(account.address)
+    }
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,42 +19,45 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-	solidity: {
-		compilers: [
-			{
-				version: "0.8.15",
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 1,
-					},
-				},
-			},
-		],
-	},
-	defaultNetwork: "hardhat",
-	networks: {
-		rinkeby: {
-			url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-			accounts: [`${process.env.PRIVATE_KEY}`],
-			gas: 3000000,
-		},
-		ropsten: {
-			url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-			accounts: [`${process.env.PRIVATE_KEY}`],
-		},
-		mumbai: {
-			url: `${process.env.ALCHEMY_API_URL}`,
-			accounts: [`${process.env.PRIVATE_KEY}`],
-		},
-		hardhat: {
-			gas: 12000000,
-			blockGasLimit: 0x1fffffffffffff,
-			allowUnlimitedContractSize: true,
-			timeout: 1800000,
-			accounts: {
-				count: 3000
-			}
-		},
-	}
-};
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.15',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1,
+                    },
+                },
+            },
+        ],
+    },
+    defaultNetwork: 'hardhat',
+    networks: {
+        rinkeby: {
+            url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+            accounts: [`${process.env.PRIVATE_KEY}`],
+            gas: 3000000,
+        },
+        ropsten: {
+            url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+            accounts: [`${process.env.PRIVATE_KEY}`],
+        },
+        mumbai: {
+            url: `${process.env.ALCHEMY_API_URL}`,
+            accounts: [`${process.env.PRIVATE_KEY}`],
+        },
+        hardhat: {
+            gas: 12000000,
+            blockGasLimit: 0x1fffffffffffff,
+            allowUnlimitedContractSize: true,
+            timeout: 1800000,
+            accounts: {
+                count: 3000,
+            },
+        },
+    },
+    etherscan: {
+        apiKey: '8VRVUMU2MXBRCD1AH5IWIW45NTZBHHVAYE',
+    },
+}
